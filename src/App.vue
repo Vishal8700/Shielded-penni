cfb620205d13fb09c72d7c07367133dbfcefa7c7
git_alien <97828106+Vishal8700@users.noreply.github.com>
2024-03-17 04:02:31 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import frontend from './views/Frontend.vue'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">frontend</RouterLink>
+
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
